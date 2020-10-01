c59b6ecfa95ac9a00f39db30850f2b17449e02c7
davidneitor185 <david.gomez.delgado@correounivalle.edu.co>
2020-10-01 15:36:02 -0500

    merge del index de Santiago

---
+++ ATodoGasWeb/ATodoGasApp/static/ATodoGasApp/js/js_Index.js
@@ -0,0 +1,38 @@
+/***************Aparecer y desaparecer Menu Lateral*****************/
+
+const btnToogle = document.querySelector('.trigger-menu');
+
+btnToogle.addEventListener('click',function(){
+    document.getElementById('Contenedor').classList.toggle('active');
+    document.getElementById('boton').classList.toggle('active');
+    
+})
+
+
+
+/***************** Scroll ***************************/
+
+const body = document.body;
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+ 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll == 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+   
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
+
