862200fad4200bdcd4a38b97f9c2ab1d9184ca3f
Victor <victor.993_@hotmail.com>
2020-10-28 14:25:29 -0500

    verifica persona antes de crear cliente

--- ATodoGasWeb/ATodoGasApp/views.py
+++ ATodoGasWeb/ATodoGasApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
 from .form import ClienteForm
+from .models import *
 
 # Create your views here.
 def login(request):
@@ -25,11 +26,16 @@ def crear_cliente(request):
         if formulario.is_valid():
             identifiacion = formulario.cleaned_data['identifiacion']
             persona = Persona.objects.filter(identifiacion__icontains=identifiacion)
-            if (perosna):
+            if (persona):
+                cliente = Cliente(idpersona=persona.idpersona)
+                cliente.save()
                 
-                
-                
-            #formulario.save()
+            else:
+                formulario.save()
+                persona = Persona.objects.filter(identifiacion__icontains=identifiacion)
+                cliente = Cliente(idpersona=persona.idpersona)
+                cliente.save()
+        
             
     return render(request, 'ATodoGasApp/crear_cliente.html',data)
 
