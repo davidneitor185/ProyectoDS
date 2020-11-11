d4c5e31a9214be6645f76543b7c189b37812c7b3
davidneitor185 <david.gomez.delgado@correounivalle.edu.co>
2020-11-11 03:20:52 -0500

    cambios funcionales en ventas

---
+++ ATodoGasWeb/ATodoGasApp/static/ATodoGasApp/js/eliminar_filap.js
@@ -0,0 +1,20 @@
+function confimarEliminacion(id){
+    Swal.fire({
+        title: '¿Está Seguro?',
+        text: "No se podra recuperar el cliente!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, Borrar!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            
+          Swal.fire(
+            'Borrado!',
+            'El cliente se ha Eliminado.',
+            'Completado'
+          )
+        }
+      })
+}
--- ATodoGasWeb/ATodoGasApp/views.py
+++ ATodoGasWeb/ATodoGasApp/views.py
@@ -4,22 +4,39 @@ from .models import *
 from django.contrib import messages
 
 # Create your views here.
+class Rowventas:
+    def __init__(self):
+        self.id =0
+        self.codigo = 0
+        self.cantidad = 0
+        self.nombreprod = ""
+        self.precio = 0
+        self.total = 0
 
 
 def login(request):
+    rowsventas.clear()
     return render(request, 'registration/login.html')
 
 
 def home(request):
+    rowsventas.clear()
     return render(request, 'ATodoGasApp/home.html')
+
+rowsventas= list()
+
 
 
 def ventas(request):
-    productos = Producto.objects.all().order_by('idproducto')
     
+    productos = Producto.objects.all().order_by('idproducto')
     encontrado = False
+    persona = Persona.objects.all()
+    existe = False
+    
     if request.method == 'POST':
-        codprod = request.POST["codProd"]
+        print("holi")
+        
         id_cliente = request.POST["id_cliente"]
         nventa = request.POST["n_venta"]
         fecha = request.POST["fecha"]
@@ -27,6 +44,14 @@ def ventas(request):
         forma_pago = request.POST["forma_pago"]
         iva = request.POST["iva"]
         estado = request.POST["estado"]
+        codprod = request.POST["codProd"]
+        cantidad = request.POST["cantidad"]
+        precio_venta = request.POST["precio_venta"]
+        descuento = request.POST["descuento"]
+
+        
+
+        
         data = {
                 "id_cliente": id_cliente,
                 "nventa": nventa,
@@ -35,11 +60,18 @@ def ventas(request):
                 "forma_pago":forma_pago,
                 "iva": iva,
                 "estado": estado,
+                "codProd":codprod,
+                "cantidad":cantidad,
+                "precio_venta":precio_venta,
+                "descuento" : descuento,
+                "tabla_cont": rowsventas
             }
         
-        if len(codprod) is 0:
-            persona = Persona.objects.all()
-            existe = False
+        
+        if 'consultar_cliente' in request.POST:
+            print("consultar cliente")  
+            
+            
             idpersona = request.POST["id_cliente"]
             for p in persona:
                 if p.identificacion == idpersona:
@@ -54,24 +86,80 @@ def ventas(request):
                 messages.error(
                 request, "Este cliente no se encuentra en nuestra base de datos, porfavor creelo")
                 return redirect(to="Crear cliente")
-        else:
+        elif 'consultar_producto' in request.POST:
+            print("consultar producto")
+             
             for p in productos:
                 if p.codigo == codprod:
                     encontrado = True
+                    
             if encontrado:
-                data ["codigo_p"] = codprod
-                data ["precioventa"] = Producto.objects.get(codigo=codprod).precioventa
+                
+                data ["precio_venta"] = Producto.objects.get(codigo=codprod).precioventa
                 data ["nombre_producto"] = Producto.objects.get(codigo=codprod).nombre_produc
+                
+                
                 return render(request, 'ATodoGasApp/ventas.html',data)
             else:
+                data ["codProd"] = None
+                data ["precio_venta"] = None
+                data ["cantidad"] = None
+                data ["descuento"] = None
                 messages.error(request, "No hay ningun producto asociado a este codigo")
                 return render(request, 'ATodoGasApp/ventas.html',data)
+        elif 'agregar' in request.POST:
+                
+            codigo = request.POST["codProd"]
+            nombre = Producto.objects.get(codigo=codigo).nombre_produc
+            cantidad = int(request.POST["cantidad"])
+            precio = Producto.objects.get(codigo=codigo).precioventa - int(request.POST["descuento"])
+            total = cantidad * precio
+
+            row = Rowventas()
+            row.id= len(rowsventas)
+            row.codigo = codigo
+            row.nombreprod = nombre
+            row.cantidad = cantidad
+            row.precio = precio
+            row.total = total
+
+            
+
+            rowsventas.append(row)
+            data["tabla_cont"] = rowsventas
+            return render(request, 'ATodoGasApp/ventas.html', data)
+        elif 'borrar' in request.POST:
+            
+
+            id_row =int( request.POST["id_row"])
+            print(id_row)
+            #print(rowsventas[0].id)           
+            #rowsventas.pop(id_row)
+            #i=0
+            #for r in rowsventas:
+            #    if r.codigo ==codigo_row:
+            #        rowsventas.remove(i)
+            #    else: i+=1
+            #if len(rowsventas) != 0:
+            #    for i in range(len(rowsventas)):
+            #        rowsventas[i].id = i
+            #data["tabla_cont"] = rowsventas
+            return render(request, 'ATodoGasApp/ventas.html', data)
+
+
+            
+
+
+            
+
+
     
 
     return render(request, 'ATodoGasApp/ventas.html')
 
 
 def consultar_cliente(request, idpersona):
+    rowsventas.clear()
     persona = Persona.objects.all()
     existe = False
     for p in persona:
@@ -91,10 +179,12 @@ def consultar_cliente(request, idpersona):
 
 
 def consultas(request):
+    rowsventas.clear()
     return render(request, 'ATodoGasApp/consultas.html')
 
 
 def crear_cliente(request):
+    rowsventas.clear()
     persona = Persona.objects.all().order_by('idpersona')
     data = {
         "form": ClienteForm(),
@@ -111,6 +201,7 @@ def crear_cliente(request):
 
 
 def modificar_cliente(request, idpersona):
+    rowsventas.clear()
     persona = Persona.objects.get(idpersona=idpersona)
     data = {
         "form": ClienteForm(instance=persona)
@@ -126,6 +217,7 @@ def modificar_cliente(request, idpersona):
 
 
 def eliminar_cliente(request, idpersona):
+    rowsventas.clear()
     persona = Persona.objects.get(idpersona=idpersona)
     persona.delete()
     return redirect(to="Crear cliente")
